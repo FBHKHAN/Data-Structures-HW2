a1bf42034509ae6a9a534dd6bf40bfea07c887fc
farooqkhan <fbhkhan92@gmail.com>
2016-02-11 10:41:44 -0500

    Initial Commit

---
+++ Assignment#2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment#2
+//
+//  Created by Farooq Khan on 2/11/16.
+//  Copyright © 2016 Farooq Khan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
